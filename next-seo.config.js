a5d4b1f28d5e452f7b2820af0ed783157d72e70b
Sergio Barria <sergio.barria@icloud.com>
2021-03-01 23:51:14 -0500

    added seo title and description

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,4 +1,7 @@
 export default {
+  title: "The Coding Script",
+  description:
+    "Web & Mobile development blog portfolio created with Next.js, Contentful and Tailwind CSS",
   openGraph: {
     type: "website",
     locale: "en_IE",
